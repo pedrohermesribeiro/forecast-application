164ffb20b9059a56e6697852fcb80e7e3738e47c
Pedro Ribeiro <pedrohermesrib@gmail.com>
2025-10-04 21:32:43 -0300

    remover configs antigos não utilizados

--- src/main/java/com/forecastapp/a/config/ChatController.java
+++
@@ -1,9 +0,0 @@
-@CrossOrigin(origins = "http://localhost:4200")
-@RestController
-public class ChatController {
-    @PostMapping("/api/ai/chat")
-    public ResponseEntity<?> handleChat(@RequestBody String message) {
-        // Handle request
-        return ResponseEntity.ok("Response");
-    }
-}
