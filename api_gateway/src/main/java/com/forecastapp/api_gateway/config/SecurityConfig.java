f001630960b5788cb8695656010ef5aefd69e2c3
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-21 15:15:35 -0300

    atualizando o arquivo SecurityConfig do api-gateway para barrar o cors

--- api_gateway/src/main/java/com/forecastapp/api_gateway/config/SecurityConfig.java
+++ api_gateway/src/main/java/com/forecastapp/api_gateway/config/SecurityConfig.java
@@ -1,36 +1,45 @@
-/*package com.forecastapp.api_gateway.config;
+package com.forecastapp.api_gateway.config;
+
+//import java.beans.Customizer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    "/auth/**",
-                    "/users/**",
-                    "/users/search/**",
-                    "/ai/**",
-                    "/chat/**",
-                    "/actuator/**"
-                ).permitAll()
-                .anyRequest().permitAll()
-            )
-            .cors(Customizer.withDefaults());
-        return http.build();
-    }
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .csrf(csrf -> csrf.disable())
+    //         .authorizeHttpRequests(auth -> auth
+    //             .requestMatchers(
+    //                 "/auth/**",
+    //                 "/users/**",
+    //                 "/users/search/**",
+    //                 "/ai/**",
+    //                 "/chat/**",
+    //                 "/actuator/**"
+    //             ).permitAll()
+    //             .anyRequest().permitAll()
+    //         )
+    //         .cors(Customizer.withDefaults());
+    //     return http.build();
+    // }
 
-    /*@Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // libera tudo
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/ai/chat", "/public/**").permitAll())
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/users", "/public/**").permitAll())
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/roles", "/public/**").permitAll())
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
@@ -57,7 +66,7 @@ public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
     }*/
 
 
-//}
+}
 
 
 
