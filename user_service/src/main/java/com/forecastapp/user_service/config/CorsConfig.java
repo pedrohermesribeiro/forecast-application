31502c4a4afb1c8abd21b5e25a54164bdb174d4a
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-24 19:55:50 -0300

    configurando o cors no arquivo application do api-gateway

--- user_service/src/main/java/com/forecastapp/user_service/config/CorsConfig.java
+++ user_service/src/main/java/com/forecastapp/user_service/config/CorsConfig.java
@@ -1,45 +1,45 @@
-package com.forecastapp.user_service.config;
+// package com.forecastapp.user_service.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-        @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")           // ← MUDAR PARA ISSO
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)               // pode continuar true
-                        .maxAge(3600);
-            }
-        };
-    }
-}
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+// @Configuration
+// public class CorsConfig {
 
-    //     @Bean
-    // public WebMvcConfigurer corsConfigurer() {
-    //     return new WebMvcConfigurer() {
-    //         @Override
-    //         public void addCorsMappings(CorsRegistry registry) {
-    //             registry.addMapping("/**")
-    //                     .allowedOrigins("http://localhost:4200") // Permita o seu frontend local
-    //                     .allowedOrigins("http://localhost:8085")
-    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-    //                     .allowedHeaders("*")
-    //                     .allowCredentials(true);
-    //         }
-    //     };
-    // }
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//         @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOriginPatterns("*")           // ← MUDAR PARA ISSO
+//                         .allowedMethods("*")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true)               // pode continuar true
+//                         .maxAge(3600);
+//             }
+//         };
+//     }
+// }
 
-//}
+
+
+//     //     @Bean
+//     // public WebMvcConfigurer corsConfigurer() {
+//     //     return new WebMvcConfigurer() {
+//     //         @Override
+//     //         public void addCorsMappings(CorsRegistry registry) {
+//     //             registry.addMapping("/**")
+//     //                     .allowedOrigins("http://localhost:4200") // Permita o seu frontend local
+//     //                     .allowedOrigins("http://localhost:8085")
+//     //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//     //                     .allowedHeaders("*")
+//     //                     .allowCredentials(true);
+//     //         }
+//     //     };
+//     // }
+
+// //}
