24b28f624ec45225e4a887790c72e5757a707c14
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-28 20:47:32 -0300

    retirando o CorsConfig do user-service

--- user_service/src/main/java/com/forecastapp/user_service/config/CorsConfig.java
+++ user_service/src/main/java/com/forecastapp/user_service/config/CorsConfig.java
@@ -1,28 +1,28 @@
-package com.forecastapp.user_service.config;
+// package com.forecastapp.user_service.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")           // ← MUDAR PARA ISSO
-                        //.allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);               // pode continuar true
-            }
-        };
-    }
-}
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//         @Override
+//         public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOriginPatterns("*")           // ← MUDAR PARA ISSO
+//                         //.allowedMethods("*")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);               // pode continuar true
+//             }
+//         };
+//     }
+// }
 
 
 
