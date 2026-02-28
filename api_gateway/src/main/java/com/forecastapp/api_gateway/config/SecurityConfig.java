d596c815e05e5413b0e0ac95e0a834fd0f6d641f
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-28 18:37:45 -0300

    removendo a classe SecurityConfig do api-gateway

--- api_gateway/src/main/java/com/forecastapp/api_gateway/config/SecurityConfig.java
+++ api_gateway/src/main/java/com/forecastapp/api_gateway/config/SecurityConfig.java
@@ -1,27 +1,27 @@
-package com.forecastapp.api_gateway.config;
+// package com.forecastapp.api_gateway.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+// @Configuration
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) // desabilita CSRF para Angular
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/ai/**").permitAll() // libera tudo em /ai
-                .anyRequest().permitAll()              // libera o resto também (temporário)
-            )
-            .formLogin(form -> form.disable())         // desliga formulário de login padrão
-            .httpBasic(basic -> basic.disable());      // desliga basic auth
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         http
+//             .csrf(csrf -> csrf.disable()) // desabilita CSRF para Angular
+//             .authorizeHttpRequests(auth -> auth
+//                 .requestMatchers("/ai/**").permitAll() // libera tudo em /ai
+//                 .anyRequest().permitAll()              // libera o resto também (temporário)
+//             )
+//             .formLogin(form -> form.disable())         // desliga formulário de login padrão
+//             .httpBasic(basic -> basic.disable());      // desliga basic auth
 
-        return http.build();
-    }
-}
+//         return http.build();
+//     }
+// }
 
 
 
