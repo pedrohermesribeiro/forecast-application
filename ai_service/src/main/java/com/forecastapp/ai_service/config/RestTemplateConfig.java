9918e5bf8b78ea2211fcfb45516414787fc1655f
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-28 21:30:40 -0300

    retirando o CrossOrigin do controller do ai-service

--- ai_service/src/main/java/com/forecastapp/ai_service/config/RestTemplateConfig.java
+++ ai_service/src/main/java/com/forecastapp/ai_service/config/RestTemplateConfig.java
@@ -1,14 +1,14 @@
-package com.forecastapp.ai_service.config;
+// package com.forecastapp.ai_service.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.client.RestTemplate;
 
-@Configuration
-public class RestTemplateConfig {
+// @Configuration
+// public class RestTemplateConfig {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
+//     @Bean
+//     public RestTemplate restTemplate() {
+//         return new RestTemplate();
+//     }
+// }
--- ai_service/src/main/java/com/forecastapp/ai_service/controller/ChatbotController.java
+++ ai_service/src/main/java/com/forecastapp/ai_service/controller/ChatbotController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.apache.commons.codec.digest.DigestUtils;
 import com.forecastapp.ai_service.util.HashUtil;
 
@@ -20,7 +20,7 @@ import com.forecastapp.ai_service.dto.ChatbotResponseDTO;
 import com.forecastapp.ai_service.security.JwtTokenUtil;
 import com.forecastapp.ai_service.service.ChatbotService;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8085", "https://forecast-frontend-gm1d.onrender.com"})
+//@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8085", "https://forecast-frontend-gm1d.onrender.com"})
 @RestController
 @RequestMapping("/ai")
 public class ChatbotController {
