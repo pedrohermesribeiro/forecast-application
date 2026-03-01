442181a4b7e43a129070ead1b7e2eba1c23d2adc
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-02-28 22:40:23 -0300

    atualizando a url do ai-service no arquivo application.yml da api-gateway novamente

--- forecast-frontend/src/app/features/ai/chat/chat.component.ts
+++ forecast-frontend/src/app/features/ai/chat/chat.component.ts
@@ -39,7 +39,7 @@ export class ChatComponent {
 
 private apiBase = environment.apiUrl;
 
-private apiUrl = 'https://api-gateway-ptj6.onrender.com/chat';
+private apiUrl = 'https://api-gateway-ptj6.onrender.com/ai/chat';
 
 
 //private apiUrl = `${environment.apiUrl}/ai/chat`;  // ou /auth/login
