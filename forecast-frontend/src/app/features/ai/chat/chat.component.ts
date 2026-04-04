5ef55282dbf633924ae604752558090b83106a6f
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-04-04 19:34:02 -0300

    incluindo a primeira respostas do IA de investimento no array mensages.

--- forecast-frontend/src/app/features/ai/chat/chat.component.ts
+++ forecast-frontend/src/app/features/ai/chat/chat.component.ts
@@ -98,15 +98,19 @@ export class ChatComponent {
         // === INVESTMENT_ADVISOR ===
         else if (this.selectedAgent === 'INVESTMENT_ADVISOR') {
           const mensagemPrincipal1 = resposta.resumo;
+          
           this.messages.push({ sender: 'bot', text: mensagemPrincipal1 }); 
           const mensagemPrincipal2 = resposta.analise;
-          this.messages.push({ sender: 'bot', text: mensagemPrincipal2 });
+          this.analise = resposta.analise;
+          //this.messages.push({ sender: 'bot', text: mensagemPrincipal2 });
           const mensagemPrincipal3 = resposta.recomendacao;
-          this.messages.push({ sender: 'bot', text: mensagemPrincipal3 });
+          //this.messages.push({ sender: 'bot', text: mensagemPrincipal3 });
           const mensagemPrincipal4 = resposta.riscos;
-          this.messages.push({ sender: 'bot', text: mensagemPrincipal4 });
+          this.riscos = resposta.riscos;
+          //this.messages.push({ sender: 'bot', text: mensagemPrincipal4 });
           const mensagemPrincipal5 = resposta.disclaimer;
-          this.messages.push({ sender: 'bot', text: mensagemPrincipal5 });
+          this.disclaimer;
+          //this.messages.push({ sender: 'bot', text: mensagemPrincipal5 });
           this.showPrediction = false;
         }
         console.log(this.messages);
